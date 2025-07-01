4f609e7d30a8dd74abcb088dde96472135000b26
Omar Tarek <omartarekmoh1@gmail.com>
2025-07-01 23:22:04 +0300

    edit employee repo to get all employee by eager loading instead of lazy loading only for the scheduler

--- src/main/java/com/example/demo/repository/EmployeeRepository.java
+++ src/main/java/com/example/demo/repository/EmployeeRepository.java
@@ -2,9 +2,16 @@ package com.example.demo.repository;
 
 import com.example.demo.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
     boolean existsByEmail(String email);
+    @Query("SELECT e FROM Employee e JOIN FETCH e.department")
+    List<Employee> findAllWithDepartments();
+
+
 }
